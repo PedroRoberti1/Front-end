1bee2cd2966c2106e9845bf574242a3016c7c751
Pedro Roberti <pedroroberti99@gmail.com>
2022-09-08 08:47:20 -0300

    agregamos angular a nuestro proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BannerComponent } from './banner/banner.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    BannerComponent,
+    UsuarioComponent,
+    SkillsComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
